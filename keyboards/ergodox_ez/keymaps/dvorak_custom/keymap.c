9465734b538bb6e61f81179fa230d1fe66fb0602
Mathieu Masy <mathieu@potloc.com>
2020-04-16 18:55:25 -0400

    Fix version macro

--- keyboards/ergodox_ez/keymaps/dvorak_custom/keymap.c
+++ keyboards/ergodox_ez/keymaps/dvorak_custom/keymap.c
@@ -280,7 +280,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if (record->event.pressed) {
     switch (keycode) {
       case VRSN:
-        SEND_STRING(QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
+        SEND_STRING(QMK_KEYBOARD " - " QMK_KEYMAP " @ " QMK_VERSION);
         return false; break;
 
       case RGB_SLD:
